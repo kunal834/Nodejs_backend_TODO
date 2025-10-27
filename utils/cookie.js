0ceb44bfb2b32b5e2ee0b41116e377dba102625d
kunal <kumarkunal8134@gmail.com>
2025-10-27 12:29:46 +0530

    solved cookie secure issuue

--- utils/cookie.js
+++ utils/cookie.js
@@ -7,7 +7,7 @@ return res.status(statuscode).cookie("token" , token , {
     httpOnly:true ,   
     expires: new Date(Date.now() + 15 * 60 * 1000) ,
     sameSite: process.env.NODE_ENV === "Development"? "lax" :"none",
-    secure: process.env.NODE_ENV === "Development"? false :"none"
+    secure: process.env.NODE_ENV === "Development"? false : true
 }).json({
     success : true,
     message : message ,
